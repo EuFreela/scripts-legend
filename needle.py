810e0b226731551a4cb677854adf1beb40c28d9c
Freelancer <32210677+EuFreela@users.noreply.github.com>
2018-12-29 18:49:17 -0200

    Update needle.py

--- needle.py
+++ needle.py
@@ -17,21 +17,9 @@ def banner():
     print "                                                             Needle v0.7"
     print "                                                                  Netcat"
 
-def wait():
-    os.system("clear")
-    banner()
-    print ( "Wait... " + str(len (success)-1) + " ports" )
-    
 def connect():
-  for x in xrange(int(ports)+1):    
-    r = commands.getoutput("nc "+str(ip)+" "+str(x))
-    if r.find("Connection refused"):
-        success.append(r)
-        wait()
-    else:
-        print 'Inject x = %d' % (x)
-
-
+    r = commands.getoutput("nc -z -v "+str(ip)+" "+str(ports) )
+    success.append(r)    
 
 
 #==============================================================================#
@@ -40,31 +28,25 @@ def connect():
 os.system("clear")
 banner()
 
-print ("Number of ports for injection(defalut 333) ::")
+print ("Number of ports (ex:1-10000) ::")
 ports = raw_input()
 print ("Define the IP for connection netcat (default 127.0.0.1) ::")
-ip = raw_input()    
+ip = raw_input()
 
 if not (ports):
-    ports = "333"
+    ports = "1-10000"
 if not (ip):
     ip = "127.0.0.1"
     
+print ("Wait.. ("+str(ports)+")")
 connect() 
 
 print "----------------------------------------"
 
-for y in xrange(len(success)):
-    success[y]
+if success:
+    print "Sucesso, ports to conection.."
+    print success
 else:    
-    print "IP "+ip+"; Ports: "+ports
-    print '\n Not success :( \n'
+    print '\n Sorry, close ports :( \n'
     
 print "----------------------------------------"
-
-
-
-
-
-  
-
